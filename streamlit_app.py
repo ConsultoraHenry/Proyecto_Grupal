90609294e382465041238aaf02991c27387b2df3
facuberon <facuberon98@gmail.com>
2023-03-23 15:40:12 -0300

    0.2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,9 @@
 
 import streamlit as st
-from google.oauth2 import service_account
-from google.cloud import bigquery
+#from google.oauth2 import service_account
+#from google.cloud import bigquery
 
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
+st.container('https://lookerstudio.google.com/embed/reporting/0b548b34-e39c-496b-a532-e938a2ccc512/page/p_n6nnehyf4c')
 
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
 
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
-
-# Print results.
-st.write("Some wise words from Shakespeare:")
-for row in rows:
-    st.write("✍️ " + row['word'])
 
