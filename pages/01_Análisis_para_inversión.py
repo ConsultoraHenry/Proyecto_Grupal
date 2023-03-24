6f459b52a1bffc2407d1441e5dc5f4849ecf5767
facuberon <facuberon98@gmail.com>
2023-03-23 23:49:55 -0300

    0.4

---
+++ pages/01_Análisis_para_inversión.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(layout="wide")
+components.iframe(src="https://lookerstudio.google.com/embed/u/0/reporting/0b548b34-e39c-496b-a532-e938a2ccc512/page/p_n6nnehyf4c", width=1250, height=600, scrolling=True)
